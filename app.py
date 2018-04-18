5b1eec12f185c3e7520f93485a0c2b8c69a5c4ff
agrgn42 <angela.schopke@gmail.com>
2018-04-17 22:06:57 -0400

    adding more layers from sources

--- app.py
+++ app.py
@@ -37,7 +37,9 @@ def index():
 	dance_sql.create_twitter_table()
 	dance_sql.create_youtube_table()
 
-	os.system("php /Users/angelaschopke/Documents/SI507/final_proj/agrgn42.github.io/dancemap/dance_mapbox.php")
+	os.system("php /Users/angelaschopke/Documents/SI507/final_proj/dancemap/flickr_mapbox.php")
+	os.system("php /Users/angelaschopke/Documents/SI507/final_proj/dancemap/twitter_mapbox.php")
+	os.system("php /Users/angelaschopke/Documents/SI507/final_proj/dancemap/youtube_mapbox.php")
 
 	# DANCE_GEOJSON = jsonify(fstring)
 	return render_template("index.html", ACCESS_KEY=MAPBOX_ACCESS_KEY)
